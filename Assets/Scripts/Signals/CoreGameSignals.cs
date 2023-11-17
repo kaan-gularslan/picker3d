9284908c32cbc819eb21e9c94c87cac1139633c9
Kaan Gülarslan <kaangularslan@gmail.com>
2023-11-17 12:05:04 +0300

    Level manager oluşturuldu

---
+++ Assets/Scripts/Signals/CoreGameSignals.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Signals
+{
+    public class CoreGameSignals : MonoBehaviour
+    {
+        public UnityAction<byte> onLevelInitialize= delegate{ };
+        public UnityAction onLevelDestroy = delegate {  };
+        public UnityAction onNextLevel = delegate {  };
+        public UnityAction onRestartLevel = delegate {  };
+        public UnityAction onClearActiveLevel = delegate {  };
+        public Func<byte> onGetLevelValue = delegate { return 0; };
+        public UnityAction onReset = delegate {  };
+        public static CoreGameSignals Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy((this.gameObject));
+                return;
+            }
+        }
+    }
+}
